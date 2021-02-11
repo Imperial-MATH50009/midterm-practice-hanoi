a06410d63885dd4e3f5ddf569cfec1ade1f04918
mattw89 <matt.woolway@gmail.com>
2021-02-11 11:48:22 +0000

    Update test_part_2.py

--- tests/test_part_2.py
+++ tests/test_part_2.py
@@ -10,8 +10,8 @@ def test_hanoi_import():
 
 
 @pytest.mark.parametrize("disks, stack", [
-    (3, [4, 3, 2]),
-    (5, [6, 5, 4, 3, 2]),
+    (3, [3, 2, 1]),
+    (5, [5, 4, 3, 2, 1]),
     (-1, [])
 ])
 def test_stack(disks, stack):
